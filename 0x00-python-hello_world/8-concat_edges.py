0af3b3d93a0379f1f60d7a8972ad286a37df8b00
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2024-03-11 23:05:18 +0200

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax"
-str = (str[str.index("object-oriented ") : str.index("language")] + str[str.index("with") : str.index("very")] + str[: str.index("is")])
+ language that combines remarkable power with very clear syntax"
+str = str[str.index("object-oriented "):str.index("language")] + str[107:112] + str[:6]
 print(str)
