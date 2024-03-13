07232e2e3e757e6366bf6139ead9ef11c7605215
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2024-03-13 16:40:56 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    print("{:02d}".format(i), end="")
-    if i < 99:
-        print(", ", end="")
+for i in range(99):
+    print("{:02d}, ".format(i), end="")
+print(i + 1)
