1c510109b66f732076a3a202dcbff19c218835ed
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2024-03-14 03:42:00 +0200

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     n_letter = ord(c)
-    if n_letter <= 97 and n_letter >= 122:
+    if n_letter >= 97 and n_letter <= 122:
         return True
     else:
         return False
