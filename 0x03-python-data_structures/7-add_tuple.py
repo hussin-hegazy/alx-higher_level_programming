a736e5ff791bab5288391a05c5063ae45feed9b3
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2024-03-18 13:02:34 +0200

    Update 7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_1 = chaek_tuple(tuple_a)
+    tuple_2 = chaek_tuple(tuple_b)
+    return (tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1])
+
+
+def chaek_tuple (tuple=()):
+    len_tuple = len(tuple)
+    if len_tuple == 0 :
+        tuple = 0, 0
+    elif len_tuple == 1 :
+        tuple = tuple[0], 0
+    else:
+        tuple = tuple[0], tuple[1]
+    return tuple
