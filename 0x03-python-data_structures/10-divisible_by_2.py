b351088d2bb7f4bb5002cdc14377c0a83d50ab0a
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2024-03-18 15:49:23 +0200

    Update 10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    Nmy_list = []
+    for n in my_list:
+        if n % 2 == 0:
+            Nmy_list.append(True)
+        else:
+            Nmy_list.append(False)
