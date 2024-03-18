2484eb8867ae0f2a01a6655d77dbcec988384179
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2024-03-18 15:08:49 +0200

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if len(my_list) == 0 :
         return None
     y = my_list[0]
-    for n in my_list:
-        if y < n:
+    for n in my_list :
+        if y < n :
             y = n
-            return y
+    return y
