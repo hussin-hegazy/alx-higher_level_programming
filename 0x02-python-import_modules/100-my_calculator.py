e38a12620a19ef67c6a9b647be5a5fc49555b508
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2024-03-19 20:48:03 +0200

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
+from sys import argv, exit
+from calculator_1 import add, sub, mul, div
 
-    if len(sys.argv) != 4:
+
+def calculate(arr):
+    if len(arr) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-    if operator == "+":
-        result = add(a, b)
-    elif operator == "-":
-        result = sub(a, b)
-    elif operator == "*":
-        result = mul(a, b)
-    elif operator == "/":
-        result = div(a, b)
-
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    print("{} {} {} = {}".format(a, operator, b, result))
+        op = (arr[1])
+        if op not in ['+', '-', '*', '/']:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+        a = int(arr[0])
+        b = int(arr[2])
+        print(a, op, b, "=", end=" ")
+        if op == '+':
+            print(add(a, b))
+        elif op == '-':
+            print(sub(a, b))
+        elif op == '*':
+            print(mul(a, b))
+        else:
+            print(div(a, b))
+    exit(0)
+
+
+if (__name__ == '__main__'):
+    calculate(argv[1:])
